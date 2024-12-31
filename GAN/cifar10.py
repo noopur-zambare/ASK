5a61977df8e68f6196e7307217053058e712f573
noopur <zambare@ualberta.ca>
2024-12-31 11:17:25 -0700

    GAN added

---
+++ GAN/cifar10.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Hyperparameters
+latent_dim = 100
+batch_size = 128
+num_epochs = 20
+learning_rate = 0.0002
+image_size = 32
+channels = 3
+
+# Data Loading and Transformation
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+# Generator
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(128, channels, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+    
+    def forward(self, z):
+        return self.model(z)
+
+# Discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(channels, 128, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, img):
+        return self.model(img).view(-1, 1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+
+optimizer_G = optim.Adam(generator.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+optimizer_D = optim.Adam(discriminator.parameters(), lr=learning_rate, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
+
+# Training
+fixed_noise = torch.randn(64, latent_dim, 1, 1, device=device)
+
+for epoch in range(num_epochs):
+    for i, (imgs, _) in enumerate(train_loader):
+        real_imgs = imgs.to(device)
+        batch_size = real_imgs.size(0)
+
+        # Train Discriminator
+        optimizer_D.zero_grad()
+        real_labels = torch.ones(batch_size, 1, device=device)
+        fake_labels = torch.zeros(batch_size, 1, device=device)
+
+        outputs = discriminator(real_imgs)
+        d_loss_real = criterion(outputs, real_labels)
+        d_loss_real.backward()
+
+        z = torch.randn(batch_size, latent_dim, 1, 1, device=device)
+        fake_imgs = generator(z)
+        outputs = discriminator(fake_imgs.detach())
+        d_loss_fake = criterion(outputs, fake_labels)
+        d_loss_fake.backward()
+
+        optimizer_D.step()
+
+        # Train Generator
+        optimizer_G.zero_grad()
+        outputs = discriminator(fake_imgs)
+        g_loss = criterion(outputs, real_labels)
+        g_loss.backward()
+        optimizer_G.step()
+
+    print(f"Epoch [{epoch}/{num_epochs}] Loss D: {d_loss_real + d_loss_fake}, Loss G: {g_loss}")
+
+    if epoch % 10 == 0:
+        with torch.no_grad():
+            fake_images = generator(fixed_noise).detach().cpu()
+        plt.figure(figsize=(8, 8))
+        plt.axis("off")
+        plt.title("Generated Images")
+        plt.imshow(np.transpose(torchvision.utils.make_grid(fake_images, padding=2, normalize=True), (1, 2, 0)))
+        plt.show()
