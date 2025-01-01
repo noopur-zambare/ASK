04d18fc0264ea12c874062d91a2ffaf58a7800b9
noopur <zambare@ualberta.ca>
2025-01-01 09:06:10 -0700

    Stabe Diffusion added

---
+++ Diffusion Models/stable_diffusion.py
@@ -0,0 +1,22 @@
+# To run this file, please install the exact same versions of the libraries
+# pip install tensorflow==2.15.0 keras==2.15.0 keras-cv==0.6.0 tensorflow-datasets==4.9.6 keras-core==0.1.7
+
+import time
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+
+
+def plot_images(images):
+    plt.figure(figsize=(20, 20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i + 1)
+        plt.imshow(images[i])
+        plt.axis("off")
+
+
+plot_images(images)
